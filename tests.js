b55bac187823791db2c8d53865dfbb3ee1ce99fd
root <root@s2.speedwatch.io>
2020-12-08 21:04:23 +0600

    working

--- tests.js
+++ tests.js
@@ -2,11 +2,13 @@ const OS = require('./index.js');
 const UA = 'TemporaryUserAgent';
 const imdb = '0898266', show = 'The Big Bang Theory', s = '01', ep = '01';
 
-let os, test;
-
+//let os, test;
+/*
 test = 'http client';
 os = new OS({
     useragent: UA,
+    username: 'ekinomaniaktv',
+    password: 'pbed27vgAdm6uyD',
     ssl: false
 });
 console.time(test);
@@ -16,6 +18,8 @@ os.api.ServerInfo().then(() => {
     test = 'https client';
     os = new OS({
         useragent: UA,
+        username: 'ekinomaniaktv',
+        password: 'pbed27vgAdm6uyD',
         ssl: true
     });
     console.time(test);
@@ -37,4 +41,71 @@ os.api.ServerInfo().then(() => {
 }).catch((err) => {
     console.log('Test failed');
     console.log(err);
+});
+*/
+
+
+
+
+const OpenSubtitles = new OS({
+    useragent:'UserAgent',
+    username: 'ekinomaniaktv',
+    password: 'pbed27vgAdm6uyD',
+    ssl: true
+});
+OpenSubtitles.login()
+    .then(res => {
+        console.log(res.token);
+        console.log(res.userinfo);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+
+
+
+    OpenSubtitles.search({
+    sublanguageid: 'fre',       // Can be an array.join, 'all', or be omitted.
+    hash: '8e245d9679d31e12',   // Size + 64bit checksum of the first and last 64k
+    filesize: '129994823',      // Total size, in bytes.
+    path: '/data/TDownloads/Black Beauty (2020) [720p] [WEBRip] [YTS.MX]/Black.Beauty.2020.720p.WEBRip.x264.AAC-[YTS.MX].mp4',        // Complete path to the video file, it allows
+                                //   to automatically calculate 'hash'.
+    filename: 'Black.Beauty.2020.720p.WEBRip.x264.AAC-[YTS.MX].mp4',        // The video file name. Better if extension
+                                //   is included.
+    season: '2',
+    episode: '3',
+    extensions: ['srt', 'vtt'], // Accepted extensions, defaults to 'srt'.
+    limit: '3',                 // Can be 'best', 'all' or an
+                                // arbitrary nb. Defaults to 'best'
+    imdbid: '528809',           // 'tt528809' is fine too.
+    fps: '23.96',               // Number of frames per sec in the video.
+    query: 'Charlie Chaplin',   // Text-based query, this is not recommended.
+    gzip: true                  // returns url to gzipped subtitles, defaults to false
+}).then(subtitles => {
+    // an array of objects, no duplicates (ordered by
+    // matching + uploader, with total downloads as fallback)
+    
+    subtitles = Object;/* {
+        en: {
+            downloads: "432",
+            encoding: "ASCII",
+            id: "192883746",
+            lang: "en",
+            langName: "English",
+            score: 9,
+            url: "http://dl.opensubtitles.org/download/subtitle_file_id",
+            filename: "some_movie.tag.srt"
+        }
+        fr: {
+            download: "221",
+            encoding: "UTF-8",
+            id: "1992536558",
+            lang: "fr",
+            langName: "French",
+            score: 6,
+            url: "http://dl.opensubtitles.org/download/subtitle_file_id",
+            filename: "some_movie.tag.srt"
+        }
+    }*/
 });
